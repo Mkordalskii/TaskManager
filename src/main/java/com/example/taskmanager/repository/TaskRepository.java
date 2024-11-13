9e07fbfa59167ac182e5ab6bc880e4903719e8fc
Maciej <m.kordalski0@gmail.com>
2024-11-13 16:34:01 +0100

    Pagination added

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -2,6 +2,13 @@ package com.example.taskmanager.repository;
 
 import com.example.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+/*Page<Task>: Spring Data JPA automatycznie obsługuje stronicowanie i zwraca wynik w postaci obiektu Page, który zawiera listę wyników i dodatkowe informacje (np. liczba stron, bieżąca strona).
+findByCompleted(boolean completed, Pageable pageable): Spring wygeneruje tę metodę automatycznie, dzięki konwencji nazw metod.
+Pageable: Parametr służący do określenia rozmiaru strony, numeru strony oraz sortowania wyników.*/
+public interface TaskRepository extends JpaRepository<Task, Long>
+{
+    Page<Task> findByCompleted(boolean completed, Pageable pageable);
 }
