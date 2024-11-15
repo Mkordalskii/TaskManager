4dfdffba387ac296fc72be9b7d261a12a2fd9d30
Maciej <m.kordalski0@gmail.com>
2024-11-15 18:30:50 +0100

    Class UserService added

---
+++ src/main/java/com/example/taskmanager/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.taskmanager.service;
+
+import com.example.taskmanager.model.User;
+import com.example.taskmanager.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService
+{
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder)
+    {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    public User saveUser(User user)
+    {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+    public User findByUsername(String username)
+    {
+        return userRepository.findByUsername(username)
+                .orElseThrow(()->new IllegalArgumentException("User not found: "+username));
+    }
+}
